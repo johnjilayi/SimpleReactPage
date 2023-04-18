cfaf4529ef554818387fc4bd12012f311ac341a6
Jonathan Njilayi <56356404+johnjilayi@users.noreply.github.com>
2023-04-18 19:44:28 -0300

    Add files via upload

---
+++ midterm/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import JonathanComp from './JonathanComp';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my React App</h1>
+      <JonathanComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ midterm/src/JonathanComp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function JonathanComp() {
+
+    const textStyle = {
+        fontSize: '45px',
+      };
+
+  return (
+    <div>
+      <h1 style={textStyle}>Jonathan Njilayi</h1>
+      <p style={textStyle}>Current program of study is Computer Science</p>
+    </div>
+  );
+}
+
+export default JonathanComp;
